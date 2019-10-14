344aae685cdc8929abab25ef5ba6eafc0d698481
migarbo1 <migarbo1@inf.upv.es>
2019-10-14 11:45:51 +0200

    estructura

---
+++ pry/tmp/a01.c
@@ -0,0 +1,7 @@
+// Ejemplo sencillo de manipulacion de vectores
+{ int a[10]; int i=0;
+
+  while (i < 10) { a[i] = i;    i++;}
+  i = 9;
+  while (i >= 0) { print(a[i]); i--;}
+}
---
+++ pry/tmp/a05.c
@@ -0,0 +1,16 @@
+// Ejemplo del uso de operadores logicos
+{ int x; int y; bool z=true;
+
+ while (z) {
+   read(x); read(y);
+   if ((x != y) || false) {
+     if (!(x == 0) && true)
+       if ((y >= 0) && (x > 0)) {
+   print(1); z = false;
+ }
+       else print(0);
+     else {}
+   }
+   else {}
+ }
+}
